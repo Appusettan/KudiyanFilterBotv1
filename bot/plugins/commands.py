39ebec2cdf35ba9f3b8665f46e8366d739664fe7
Appusettan <86650055+Appusettan@users.noreply.github.com>
2021-06-29 15:36:12 +0530

    Update commands.py

--- bot/plugins/commands.py
+++ bot/plugins/commands.py
@@ -30,7 +30,7 @@ async def start(bot, update):
             await bot.send_document(
                 chat_id=update.chat.id,
                 document = file_id,
-                caption = caption,
+                caption = @⚔꧁༺⁣സിനിമ കോട്ട༻꧂⚔",
                 parse_mode="html",
                 reply_to_message_id=update.message_id,
                 reply_markup=InlineKeyboardMarkup(
@@ -38,7 +38,7 @@ async def start(bot, update):
                         [
                             InlineKeyboardButton
                                 (
-                                    'Developers', url="https://t.me/CrazyBotsz"
+                                    '🏅Our Group🏅', url="https://t.me/joinchat/70yN4sj6cjhmMTA9"
                                 )
                         ]
                     ]
